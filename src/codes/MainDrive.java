1c1764e2d4c11e80d3109c3a89fc345baf8f593a
이아현 <suk03029@gamil.com>
2021-11-11 16:50:45 +0900

    검사 통과시 문제 숫자로 산정 및 출력

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -18,10 +18,9 @@ public class MainDrive {
 //				1~9중 하나를 랜덤으로 추출
 				int randomNum = (int) (Math.random() * 9 + 1);
 
-				
 //				중복 검사. 같은 값이 있는지?
 				boolean isDupOk = true;
-				
+
 				for (int cpuNum : cpuNumbers) {
 					if (randomNum == cpuNum) {
 //						//여기있는 수는 사용하면 안됨
@@ -31,7 +30,19 @@ public class MainDrive {
 				}
 
 //				써도 되는 숫자면, 집어넣고 아니면 넘어가자
+
+				if (isDupOk) {
+					cpuNumbers[i] = randomNum;
+					break;
+
+				}
 			}
+		}
+		
+		
+//		나온 문제가 어떤 값인지 출력 //////////////////////////Sammi question+배열 출력은 꼭 for여야 하는가?
+		for(int cpuNum : cpuNumbers) {
+			System.out.println(cpuNum);
 		}
 
 	}
