f1d89b9fbe0499f8f35c5689ddd8251d6afbe2b2
이아현 <suk03029@gamil.com>
2021-11-11 16:42:57 +0900

    cpu가 문제를 출제 하는것
    맞는 숫자가 나올때 까지 계속 뽄는구조를 만듬

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -4,6 +4,22 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 
+//		컴퓨터가 3자리 숫자 문제 출제
+//		문제는 3칸의 배열로 만들것임
+		int[] cpuNumbers = new int[3];
+		
+//		하나의 칸 마다 랜덤 숫자를 채울 것임
+		
+		for(int i=0; i<cpuNumbers.length; i++) {
+			
+//			써도 되는 숫자가 나올 때 까지 무한 반복
+			
+			while(true) {
+				
+//				써도 되는 숫자면, 집어넣고 아니면 넘어가자
+			}
+		}
+		
 	}
 
 }
