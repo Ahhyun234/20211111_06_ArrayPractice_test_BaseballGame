23736c67de01afaf2fdca52f404380031e4690a9
이아현 <suk03029@gamil.com>
2021-11-11 16:47:16 +0900

    랜덤으로 숫자를 뽑고 중복검사

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -7,19 +7,33 @@ public class MainDrive {
 //		컴퓨터가 3자리 숫자 문제 출제
 //		문제는 3칸의 배열로 만들것임
 		int[] cpuNumbers = new int[3];
-		
+
 //		하나의 칸 마다 랜덤 숫자를 채울 것임
-		
-		for(int i=0; i<cpuNumbers.length; i++) {
-			
+
+		for (int i = 0; i < cpuNumbers.length; i++) {
+
 //			써도 되는 숫자가 나올 때 까지 무한 반복
-			
-			while(true) {
+
+			while (true) {
+//				1~9중 하나를 랜덤으로 추출
+				int randomNum = (int) (Math.random() * 9 + 1);
+
 				
+//				중복 검사. 같은 값이 있는지?
+				boolean isDupOk = true;
+				
+				for (int cpuNum : cpuNumbers) {
+					if (randomNum == cpuNum) {
+//						//여기있는 수는 사용하면 안됨
+						isDupOk = false;
+
+					}
+				}
+
 //				써도 되는 숫자면, 집어넣고 아니면 넘어가자
 			}
 		}
-		
+
 	}
 
 }
